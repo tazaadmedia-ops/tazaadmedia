f2e67fb8672afc35043c7158ab02493006dfa883
Kamran Wahid <tazaadmedia@gmail.com>
2026-03-05 16:24:18 +0500

    fix: remove unused React import in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense, lazy } from 'react';
+import { Suspense, lazy } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import LoadingSpinner from './components/LoadingSpinner';
